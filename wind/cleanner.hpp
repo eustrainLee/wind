ce2dda012b9f74b8edfc35a6000c16276cbc5967
eustrainLee <eustrain@outlook.com>
2022-06-02 12:09:40 +0800

    allow exception in wind::cleanner

--- wind/cleanner.hpp
+++ wind/cleanner.hpp
@@ -3,28 +3,69 @@
 
 #include <functional>
 #include <memory>
+#include <utility>
 
 namespace wind {
     class cleanner {
-        struct task {
-            ::std::function<void()> clean;
-            ::std::unique_ptr<task> next;
-            ~task() { clean(); }
+    public:
+        ~cleanner() noexcept(false) = default;
+
+    private:
+        struct garbage;
+
+        class unique_garbage {
+        public:
+            unique_garbage() : p{nullptr} {}
+
+            template<typename Callable>
+            unique_garbage(Callable&& callable, unique_garbage&& next)
+                : p{new garbage{::std::forward<Callable>(callable), ::std::move(next)}} { }
+
+            unique_garbage(const unique_garbage&) = delete;
+
+            unique_garbage(unique_garbage&& oth) : p{::std::exchange(oth.p, nullptr)} { }
+
+            unique_garbage& operator=(const unique_garbage& rhs) = delete;
+
+            unique_garbage& operator=(unique_garbage&& rhs) {
+                if(this != &rhs) {
+                    if(p) { delete p; p = nullptr; }
+                    p = ::std::exchange(rhs.p, nullptr);
+                }
+                return *this;
+            }
+
+            ~unique_garbage() noexcept(false) {
+                if(p) { delete p; p = nullptr; }
+            }
+
+        private:
+            garbage* p;
+
         };
-        ::std::unique_ptr<task> tasks;
+        
+        struct garbage {
+            ::std::function<void()> clean;
+            unique_garbage next;
+            ~garbage() noexcept(false) { clean(); }
+        };
+
+        unique_garbage garbages;
+
         template<typename Callable, typename = decltype(::std::declval<Callable>()())>
         friend cleanner& operator << (cleanner& c, Callable&& callable) {
-            ::std::unique_ptr<task> new_task = ::std::unique_ptr<task>{new task{::std::forward<Callable>(callable), ::std::move(c.tasks)}};
-            c.tasks = ::std::move(new_task);
+            unique_garbage new_garbage{::std::forward<Callable>(callable), ::std::move(c.garbages)};
+            c.garbages = ::std::move(new_garbage);
             return c;
         }
     };
 }
 
-#ifdef ALLOW_WIND_CLEANNER_MACRO_
+#ifdef ALLOW_WIND_CLEANNER_MACRO
 
 #define DISCARD(CLEANNER) CLEANNER << [&]()
+#define DISCARD_COPY(CLEANNER) CLEANNER << [=]()
 
-#endif // ALLOW_WIND_CLEANNER_MACRO_
+#endif // ALLOW_WIND_CLEANNER_MACRO
 
 #endif
